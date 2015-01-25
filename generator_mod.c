0017da67c7c1671b2512ec532ed6ae7402f88ec3
Piotr Bogdan <bogdzio@o2.pl>
2015-01-25 19:34:59 +0000

    Failed to compile, Galileo's Yocto lacking kernel headers

---
+++ generator_mod.c
@@ -0,0 +1,23 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+static int my_init(void)
+{
+	int i;
+	printk(KERN_ALERT "Modul z bledem 1\nZaczynam inicjalizacje\n");	
+	printk(KERN_ALERT "Inicjalizacja zakonczona\n");
+	return 0;
+}
+static void my_exit(void)
+{
+	if(table) kfree(table);
+	printk(KERN_ALERT "Do widzenia\n");
+}
+
+MODULE_LICENSE("GPL");
+
+module_init(my_init);
+module_exit(my_exit);
+
+
