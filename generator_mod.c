40c6634fc270187b54d267d46708c6dc732bda4b
Piotr Bogdan <bogdzio@o2.pl>
2015-01-27 23:26:26 +0000

    First version able to compile, insmod and printk some text

--- generator_mod.c
+++ generator_mod.c
@@ -4,14 +4,12 @@
 
 static int my_init(void)
 {
-	int i;
 	printk(KERN_ALERT "Modul z bledem 1\nZaczynam inicjalizacje\n");	
 	printk(KERN_ALERT "Inicjalizacja zakonczona\n");
 	return 0;
 }
 static void my_exit(void)
 {
-	if(table) kfree(table);
 	printk(KERN_ALERT "Do widzenia\n");
 }
 
