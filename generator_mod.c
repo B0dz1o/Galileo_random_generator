4ce0d0d9d7fb88f16bb099026283addaba58a1ed
B0dz1o <b0dzitsuu@gmail.com>
2015-01-28 03:21:41 +0100

    testing loop baser on timer

--- generator_mod.c
+++ generator_mod.c
@@ -1,15 +1,38 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/slab.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
+
+struct timer_list myTimer;
 
 static int my_init(void)
 {
-	printk(KERN_ALERT "RAND_GEN->initialized\n");
+	printk(KERN_INFO "RAND_GEN->initialized\n");
+	if(!prepare_timer()){
+		printk(KERN_ALERT "RAND_GEN->unable to initialize timer!");
+	}
 	return 0;
 }
 static void my_exit(void)
 {
-	printk(KERN_ALERT "RAND_GEN->removed module\n");
+	printk(KERN_INFO "RAND_GEN->removed module\n");
+}
+
+int prepare_timer(){
+	static long int loopIndex = 0;
+	init_timer(&myTimer);
+	mt.data = ++loopIndex;
+	// call every second
+	myTimer.expires(jiffies + HZ);
+	mt.function = read_gpio;
+	add_timer(&mt);
+}
+
+void read_gpio(unsigned long data){
+	printk(KERN_INFO "Timer loop%d",data);
+	if(!prepare_timer()){
+		printk(KERN_ALERT "RAND_GEN->unable to initialize timer!");
+	}
 }
 
 MODULE_LICENSE("GPL");
