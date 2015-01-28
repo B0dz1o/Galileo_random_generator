95339683afcfe0717a62746770fcfc1d03c12cb1
Piotr Bogdan <bogdzio@o2.pl>
2015-01-28 03:49:10 +0000

    after compilation, module is successfully inserted and can be safely removed 	modified:   generator_mod.c

--- generator_mod.c
+++ generator_mod.c
@@ -1,22 +1,19 @@
 #include "generator_mod.h"
 
 struct timer_list myTimer;
-int keep_updating;
 
 static int my_init(void)
 {
-	keep_updating = 1;
 	printk(KERN_INFO "RAND_GEN->module inserted\n");
 	//check non-zero return
 	if(prepare_timer()){
-		printk(KERN_ALERT "RAND_GEN->unable to initialize timer!");
+		printk(KERN_ALERT "RAND_GEN->unable to initialize timer!\n");
 	}
 	printk(KERN_INFO "RAND_GEN->initialized timer\n");
 	return 0;
 }
 static void my_exit(void)
 {
-	keep_updating = 0;
 	del_timer(&myTimer);
 	printk(KERN_INFO "RAND_GEN->removed module\n");
 }
@@ -35,10 +32,7 @@ int update_timer(){
 }
 
 void read_gpio(unsigned long data){
-	printk(KERN_INFO "Timer loop");
-	if(keep_updating && !update_timer()){
-		printk(KERN_ALERT "RAND_GEN->unable to update timer!");
-	}
+	printk(KERN_INFO "Timer loop\n");
 }
 
 MODULE_LICENSE("GPL");
