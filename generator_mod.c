b47dd966621bf3ed800748ffa6315a85e2f6eace
B0dz1o <b0dzitsuu@gmail.com>
2015-01-28 02:53:42 +0100

    libpthread bug in Debian/Yocto combination -> MRAA library cannot be utilised new workflow

--- generator_mod.c
+++ generator_mod.c
@@ -4,18 +4,15 @@
 
 static int my_init(void)
 {
-	printk(KERN_ALERT "Modul z bledem 1\nZaczynam inicjalizacje\n");	
-	printk(KERN_ALERT "Inicjalizacja zakonczona\n");
+	printk(KERN_ALERT "RAND_GEN->initialized\n");
 	return 0;
 }
 static void my_exit(void)
 {
-	printk(KERN_ALERT "Do widzenia\n");
+	printk(KERN_ALERT "RAND_GEN->removed module\n");
 }
 
 MODULE_LICENSE("GPL");
 
 module_init(my_init);
 module_exit(my_exit);
-
-
