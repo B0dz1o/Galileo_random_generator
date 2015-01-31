5956d4052f9d1689197f6edc426577f1fe454a58
Piotr Bogdan <bogdzio@o2.pl>
2015-01-31 00:39:06 +0000

    Another batch of changes

--- generator_mod.c
+++ generator_mod.c
@@ -4,12 +4,10 @@ struct timer_list myTimer;
 
 static int my_init(void)
 {
-	printk(KERN_INFO "RAND_GEN->module inserted\n");
 	//check non-zero return
 	if(prepare_timer()){
 		printk(KERN_ALERT "RAND_GEN->unable to initialize timer!\n");
 	}
-	init_gpio();
 	printk(KERN_INFO "RAND_GEN->initialized timer\n");
 	return 0;
 }
@@ -17,7 +15,6 @@ static void my_exit(void)
 {
 	del_timer(&myTimer);
 	printk(KERN_INFO "RAND_GEN->removed module\n");
-	gpio_free(19);
 }
 
 int prepare_timer(){
@@ -33,32 +30,48 @@ int update_timer(){
 	return mod_timer(&myTimer, jiffies + HZ);
 }
 
-void init_gpio(){
-	struct file *fd;
-}
-
 void read_gpio(unsigned long data){
-	static int onOff = 0;
-	int xJoy,yJoy,iR;
-	struct file *fd;
-	char label[32];
-	int result = gpio_request(19,"PINSET");
-	gpio_direction_output(19,1);
-	fd = filp_open("/sys/bus/iio/devices/iio\:device0/in_voltage0_raw",O_RDONLY,0);
-	fd->f_op->read(fd,label,5,0);	
-	filp_close(fd,NULL);
-
-	printk(KERN_INFO "val:%s\n",label);
-	add_entropy(1,2,3);
+	int analogValue;
+	analogValue = read_analog(37,0);
+	printk(KERN_INFO "analog value:%d\n",analogValue);
 	update_timer();
 }
 
-void add_entropy(int xJoy, int yJoy, int iR){
+int read_analog(unsigned int gpio_allow, int analog_src){
+	char analog_value[8];
+	int errValue;
+	errValue = gpio_request(gpio_allow,"AD_ENABLE");
+	if (errValue == 0){
+		errValue = gpio_direction_output(gpio_allow,1);
+		if(errValue == 0){
+			struct file *fd;
+			char path[64];
+			sprintf(path,"/sys/bus/iio/devices/iio\\:device0/in_voltage%d_raw",analog_src);
+			fd = filp_open(path,O_RDONLY,0);
+			errValue = (int) fd;
+			if(fd == 0){
+				fd->f_op->read(fd,analog_value,5,0);
+				filp_close(fd,NULL);
+			}
+			gpio_free(gpio_allow);
+		}
+	}
+	if (errValue != 0){
+		printk(KERN_ALERT "Analog read error:%d\n",errValue);
+		return errValue;
+	} else{
+		int retValue;
+		char** endPoint = NULL;
+		const int base = 10;
+		retValue = (int) simple_strtol (analog_value,endPoint,base);
+		return(retValue);
+	}
+}
+
+void add_entropy(int entSource){
 	struct file *fd;
 	fd = filp_open("/dev/random",O_WRONLY,0);
-	fd->f_op->unlocked_ioctl(fd,RNDADDENTROPY,xJoy);
-	fd->f_op->unlocked_ioctl(fd,RNDADDENTROPY,yJoy);
-	fd->f_op->unlocked_ioctl(fd,RNDADDENTROPY,iR);
+	fd->f_op->unlocked_ioctl(fd,RNDADDENTROPY,entSource);
 	filp_close(fd,NULL);
 	return;
 }
@@ -67,3 +80,4 @@ MODULE_LICENSE("GPL");
 
 module_init(my_init);
 module_exit(my_exit);
+
