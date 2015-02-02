6c2ceba51d2a3cfcc401fdcf4a3061d6ca681e3f
Piotr Bogdan <bogdzio@o2.pl>
2015-02-02 22:24:53 +0100

    Completely got rid of all not recommended functions (vide filp_open), that make use of user context. Still didn't manage to get any reading from analog inputs.

--- generator_mod.c
+++ generator_mod.c
@@ -6,122 +6,94 @@ author: Piotr Bogdan
 
 #include "generator_mod.h"
 
-struct hwrng* this_device;
+struct hwrng this_device;
+struct iio_map* map_analog;
+struct iio_channel* channel_analog;
 
 static int my_init(void)
 {	
-	int gpio_allow = 37,errValue;
-	this_device->name = "A0_till_A2";
-//	this_device->quality = 100;
+	this_device.name = "A0_till_A2";
+//	this_device.quality = 100;
 ///quality field is not present in 3.8.* kernel
-	this_device->priv = 17000;
-	this_device->read = read_random;
-	
-	hwrng_register(this_device);
-
-	printk(KERN_ALERT "RAND_GEN->initialized timer\n");
-	errValue = gpio_request_one(gpio_allow,GPIOF_INIT_LOW,"ALLOW_ADC");
-	printk(KERN_ALERT "%d gpio returned %d\n",gpio_allow,errValue);
-	gpio_free(gpio_allow);
-
-	gpio_allow = 26;
-	errValue = gpio_request(gpio_allow,"ALLOW_ADC");	
-	printk(KERN_ALERT "%d gpio returned %d\n",gpio_allow,errValue);
-	errValue = gpio_direction_output(26,1);
-	gpio_set_value(gpio_allow, 1);
-	gpio_free(26);	
-	printk(KERN_INFO "turned output high, code:%d\n",errValue);
+	this_device.read = read_hw_random;
+	hwrng_register(&this_device);
+	printk(KERN_INFO "RAND_GEN->inserted module\n");
 	return 0;
 }
 static void my_exit(void)
 {	
-	int gpio_allow = 26,errValue;
-	hwrng_unregister(this_device);
-	gpio_free(26);
-	errValue = gpio_request(gpio_allow,"ALLOW_ADC");	
-	printk(KERN_ALERT "%d gpio returned %d\n",gpio_allow,errValue);
-	errValue = gpio_direction_output(26,0);
-	gpio_set_value(gpio_allow, 0);
-	printk(KERN_INFO "turned output high, code:%d\n",errValue);
-	gpio_free(26);	
-
-	///remove timer to break from loop
-
-//	gpio_free(26);
+	hwrng_unregister(&this_device);
 	printk(KERN_INFO "RAND_GEN->removed module\n");
-}
-
-void read_gpio(unsigned long data){
-	///check 3 analog inputs and read data from them in case of non-error return
-	int analogValue;
-	analogValue = read_analog(37,0); ///read from A0
-	add_entropy(17);
-	analogValue = read_analog(36,1); ///read from A1
-	analogValue = read_analog(23,2); ///read from A1
-/*	if (analogValue >= 0) {
-		add_entropy(analogValue);
-	}
-	analogValue = read_analog(36,1); ///read from A1
-	if (analogValue >= 0) {
-		add_entropy(analogValue);
-	}
-	analogValue = read_analog(23,2); ///read from A2
-	if (analogValue >= 0) {
-		add_entropy(analogValue);
-	}
-*/
-	///continue working, repeat every second
 }
 
 int read_analog(unsigned int gpio_allow, int analog_src){
 	///analog ports all have alternative functions; to use them as inputs
 	///certain gpio ports must be set to "output 0"
-	char analog_value[8];
+	///also, to have some visible effect, the in-board diode blink whenever this
+	///function is called from hw_random driver
+	const int diode_gpio = 3;
 	int errValue;
-//	struct file *fd;
-//	char path[64];
-	errValue = gpio_request_one(gpio_allow,GPIOF_INIT_LOW,"ALLOW_ADC");
-	printk(KERN_INFO "%d requested : %d\n",gpio_allow,errValue);
+	char analog_value[8];
+	gpio_request(diode_gpio,"diode");
+	gpio_direction_output(diode_gpio,1);
+	gpio_set_value(diode_gpio,1);
+	errValue = gpio_request(gpio_allow,"ALLOW_ADC");
+	printk(KERN_ALERT "Request:%d\n",errValue);
 	if (errValue == 0){
-//		errValue = gpio_direction_output(gpio_allow, 0);
+		errValue = gpio_direction_output(gpio_allow, 0);
+		printk(KERN_ALERT "Direction output:%d\n",errValue);
 		if(errValue == 0){
-			printk(KERN_INFO "%d requested : %d\n",gpio_allow,errValue);
+			gpio_set_value(gpio_allow,0);	
+//ALL THE MAGIC IS HERE
+
+
+
+
+
+
+
+
+
+
+
+//END OF MAGIC
 		}
 	}
+	gpio_set_value(diode_gpio,0);
+	gpio_free(diode_gpio);
 	gpio_free(gpio_allow);
 	///prepare path to read from chosen analog input, A0-A2
 	if (errValue != 0){
 		///in case of error, send through its code
 		printk(KERN_ALERT "Analog read error:%d\n",errValue);
 		return errValue;
-	} else{
-		///if no error, convert char* analog output to int, which can be passed
-		///to entropy pool
-		int retValue;
-		char** endPoint = NULL; ///simple_strtol returns pointer to the last digit read
-		const int base = 10; ///numeric system base
-		retValue = (int) simple_strtol (analog_value,endPoint,base);
-		return retValue;
-	}
+	} else	
+		printk(KERN_ALERT "Read value:%s\n",analog_value);
+		return 0;
 }
 
-void add_entropy(int entSource){
-	///add new data to system entropy pool, using predefined ioctl interface
-	struct file *fd;
-	fd = filp_open("/dev/random",O_WRONLY,0);
-	if(fd >= 0){
-		///successful file descriptor open
-		fd->f_op->unlocked_ioctl(fd,RNDADDENTROPY,entSource);
-		filp_close(fd,NULL);
-	}
-	return;
-}
 
-int read_random(struct hwrng *rng, void* data, size_t max, bool wait) {
-	int myData[] = {1,7,30,452};
-	printk(KERN_INFO "data read\n");	
+int read_hw_random(struct hwrng *rng, void* data, size_t max, bool wait) {
+	int myData[3];
+	int count = 0,val;
+	val = read_analog(37,0);
+	if(!val){
+		myData[count] = val;
+		++count;
+	}
+	val = read_analog(36,1);
+	if(!val){
+		myData[count] = val;
+		++count;
+	}
+	val = read_analog(23,2);
+	if(!val){
+		myData[count] = val;
+		++count;
+	}
+	printk(KERN_INFO "data read:%d\n",count);	
 	data = myData;
-	return 32;
+	return count * sizeof(int);
 }
 
 MODULE_LICENSE("GPL");
